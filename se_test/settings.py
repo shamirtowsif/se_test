7ec7cc5a0f6a072bb142824e47411ccde9fb4a9f
Shamir Towsif <shamir.towsif@protonmail.com>
2026-01-29 06:19:19 -0600

    A new model Video

--- se_test/settings.py
+++ se_test/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -74,11 +75,14 @@ WSGI_APPLICATION = 'se_test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'se_test',
+        'USER': 'se_shamir',
+        'PASSWORD': 'se_pass',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
