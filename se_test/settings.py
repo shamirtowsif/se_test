78c2601caccc55c886d7f4912bd1db37068fa595
Shamir Towsif <shamir.towsif@protonmail.com>
2026-04-02 01:02:57 -0500

    csrf token

--- se_test/settings.py
+++ se_test/settings.py
@@ -136,3 +136,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://se-test-app-205073294860.us-central1.run.app"
+]
