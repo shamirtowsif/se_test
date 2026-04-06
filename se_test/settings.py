2a2c4f7e8a45b9b1b442777d43a7c6f20fa36502
Shamir Towsif <shamir.towsif@protonmail.com>
2026-04-06 14:03:27 -0500

    change in settings

--- se_test/settings.py
+++ se_test/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,16 +75,37 @@ WSGI_APPLICATION = 'se_test.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'se_test',
-        'USER': 'se_shamir',
-        'PASSWORD': 'se_pass',
-        'HOST': 'db',
-        'PORT': '3306',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'se_test',
+#         'USER': 'se_shamir',
+#         'PASSWORD': 'se_pass',
+#         'HOST': 'db',
+#         'PORT': '3306',
+#     }
+# }
+
+USE_SQLITE = os.environ.get("USE_SQLITE", "False") == "True"
+
+if USE_SQLITE:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": "se_test",
+            "USER": "se_shamir",
+            "PASSWORD": "se_pass",
+            "HOST": "db",
+            "PORT": "3306",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
